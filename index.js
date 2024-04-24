12b0252b11829a695de56ed23d925edaaf74c61f
Aminata-Colle12Diagne <diagneaminata464@gmail.com>
2024-04-24 13:19:20 +0000

    ajout de middleware

--- index.js
+++ index.js
@@ -1,42 +1,24 @@
-// Add Express
 const express = require("express");
-
-// Initialize Express
 const app = express();
 
+// Middleware to parse JSON bodies
+app.use(express.json());
 
+// Connection to database
 require('./db/connection');
 const Users = require('./Model/User');
-// const UserModal = require('./Model/Data');
 
+// POST route to handle incoming data
 app.post("/", async (req, res) => {
     let user = new Users(req.body);
     let result = await user.save();
     res.send(result);
 });
 
-// Create GET request
-// app.get("/", (req, res) => {
-//   res.send("Express on Vercel");
-// });
-
-// ajout hotel
-// app.post("/addHotel", async (req, res) => {
-//     const hotelData = req.body;
-
-//     try {
-//         const newHotel = new UserModal(hotelData);
-//         await newHotel.save();
-//         res.json(newHotel);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).send("Erreur lors de l'ajout des données de l'hôtel");
-//     }
-// });
-
 // Initialize server
 app.listen(5000, () => {
   console.log("Running on port 5000.");
 });
+
 // Export the Express API
 module.exports = app;
